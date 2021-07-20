20216b119203f1b69f8f412bafeaed869cd60b32
Kevin P. Fleming <kpfguitar77@gmail.com>
2021-07-20 11:39:32 -0400

    SEEDER function created

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ const app = express();
 
 app.use(express.json());
 
+
+
 // Import all Routes
 const products = require('./routes/product');
 
 
-app.use('/api/v1', products)
+app.use('/api/v1', products);
 
 module.exports = app;
