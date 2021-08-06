ce2cec3ad3cfae807d0ebaa67b71e4c532eb6e9c
Kevin P. Fleming <kpfguitar77@gmail.com>
2021-08-06 11:08:03 -0400

    Added Pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,13 +16,18 @@ exports.newProduct = catchAsyncErrors (async(req, res, next) => {
 // Get ALL products at /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors (async(req, res, next) => {
 
+    const resPerPage = 4;
+    const productCount = await Product.countDocuments();
     const apiFeatures = new APIFeatures(Product.find(), req.query)
                 .search()
+                .filter()
+                .pagination(resPerPage);
 
     const products = await apiFeatures.query;
     res.status(200).json({
         success: true,
         count: products.length,
+        productCount,
         products
     })
 })
@@ -72,3 +77,4 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
         message: "Product has been deleted"
     })
 })
+
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -8,14 +8,39 @@ class APIFeatures {
         const keyword = this.queryStr.keyword ? {
             name: {
                 $regex: this.queryStr.keyword,
-                //  'i' = case insensitive
                 $options: 'i' 
+                //  'i' = Case insensitive     
             }
         } : {}
 
         console.log(keyword);
 
         this.query = this.query.find({ ...keyword});
+        return this;
+    }
+    filter() {
+        const queryCopy = { ...this.queryStr};
+        console.log(queryCopy);
+        // removing fields from the query
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+        console.log(queryCopy);
+
+        // Advance filter for price, ratings, etc...
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}` );
+
+        console.log(queryStr);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+    pagination(resPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+        // BELOW: calulates how many results to skip based on what page you are on.  Based on resPerPage = 10
+        const skip = resPerPage * (currentPage - 1);
+        // BELOW: use limit and skip to create a controlled pagination
+        this.query = this.query.limit(resPerPage).skip(skip);
         return this;
     }
 }
