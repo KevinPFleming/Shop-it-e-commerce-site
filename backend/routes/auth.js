64dfa44c00f975a10a35a58a10063267e174e291
kevinpfleming <kevin.patrick.fleming@gmail.com>
2021-08-31 18:27:16 -0400

    finishing server

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,8 +20,8 @@ router.route('/login').post(loginUser);
 router.route('/logout').get(logout);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
-router.route('/me').get(isAuthenticatedUser, getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
 router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
 router.route('/admin/user/:id')
